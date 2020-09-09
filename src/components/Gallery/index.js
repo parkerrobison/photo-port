9050e88382b01270da5e5510cb2a2e604d39863a
probison <parker.robison@hotmail.com>
2020-09-08 22:01:51 -0600

    added PhotoList component with tests for it and Gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers'
-import photo from "../../../src/assets/small/commercial/0.jpg"
+import PhotoList from '../PhotoList'
 
-function Gallery(props) {
-    const currentCategory = {
-        name: "commercial",
-        description:
-          "Photos of grocery stores, food trucks, and other commercial projects",
-      };
+
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
     return(
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.description}</p>
-            <img
-                src={photo}
-                alt='Commercial Example'
-                className ='image-thumbnail mx-1'
-            />
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
